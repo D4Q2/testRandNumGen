7f6673fd0b897d880a3923cad4b42e626e49c78a
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-04-12 19:09:13 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-var odd = [ 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5 ]
-var even = [ 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6 ]
-var neither = [ 7, 7, 8, 8, 9 ]
-function generateNewNumber() {
+var odd = [ '1', '3', '5' ];
+var even = [ '2', '4', '6' ];
+var neither = [ '7', '8', '9' ];
+
+// Generate a new list of moves
+function generateNewMove() {
   // Declare some variables
-  var strokes = "";
-  var lastStroke = 1;
-  var newStroke = 1;
+  var strokes = '';
+  var lastStroke = '';
+  var newStroke = '';
   // Get the length
   var length = parseInt(document.getElementById("length").value);
  
-  // Generate a new number based on parameters for the length
+  // Generate a new number (based on parameters) for the length
   for (var i = 0; i < length; i++) {
     while (lastStroke == newStroke) {
-      // Check if the number is even
-      if (lastStroke % 2 == 0) {
+      // Check if the last stroke was even
+      if (even.contains(lastStroke)) {
         var sel =  Math.floor(Math.random() * (odd.length + neither.length));
         newStroke = sel < odd.length ? odd[sel] : neither[sel - even.length];
       }
@@ -22,9 +24,11 @@ function generateNewNumber() {
         newStroke = sel < even.length ? even[sel] : neither[sel - even.length];
       }
     }
-    
+    // Set the last stroke
     lastStroke = newStroke;
-  	strokes = strokes.concat(" ", newStroke.toString());
+    // Add the stroke to the list
+  	strokes = strokes.concat(' ', newStroke);
   }
+  // Output the strokes
   document.getElementById("numberDiv").innerHTML = strokes;
 }
