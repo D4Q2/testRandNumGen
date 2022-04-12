432fcf8fa99f07ce37894b69df94bea435187033
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-04-12 16:37:21 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-void generateNewNumber() {
-  alert("Test!");
+var lastStroke = 0;
+var strokes = 'Number: ';
+function generateNewNumber() {
+  lastStroke += 1;
+  strokes = (lastStroke.toString());
+  document.getElementById("numberDiv").innerHTML = strokes;
 }
