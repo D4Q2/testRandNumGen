474122d454c2f071eb4f9d086526305d7cab3192
D4Q2 <61206792+D4Q2@users.noreply.github.com>
2022-04-12 17:23:51 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-var lastStroke = 0;
-var strokes = 'Number: ';
+var odd = [ 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5, 1, 3, 5 ]
+var even = [ 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6, 2, 4, 6 ]
+var neither = [ 7, 7, 8, 8, 9 ]
 function generateNewNumber() {
-  lastStroke += 1;
-  strokes = (lastStroke.toString());
+  // Declare some variables
+  var strokes = "";
+  var lastStroke = 1;
+  var newStroke = 1;
+  // Get the length
+  var length = parseInt(document.getElementById("length").value);
+ 
+  // Generate a new number based on parameters for the length
+  for (var i = 0; i < length; i++) {
+    while (lastStroke == newStroke) {
+      // Check if the number is even
+      if (lastStroke % 2 == 0) {
+        var sel =  Math.floor(Math.random() * (odd.length + neither.length));
+        newStroke = sel < odd.length ? odd[sel] : neither[sel - even.length];
+      }
+      else {
+        var sel =  Math.floor(Math.random() * (even.length + neither.length));
+        newStroke = sel < even.length ? even[sel] : neither[sel - even.length];
+      }
+    }
+    
+    lastStroke = newStroke;
+  	strokes = strokes.concat(" ", newStroke.toString());
+  }
   document.getElementById("numberDiv").innerHTML = strokes;
 }
